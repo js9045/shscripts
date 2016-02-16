71b55d256f54298cb80cf65322712d50dc056845
Daniel Alvarez <js9045@gmail.com>
2016-02-16 13:28:09 -0800

    Add options for number of times to loop and delay interval

--- sendrecords.sh
+++ sendrecords.sh
@@ -14,15 +14,35 @@
 # not clear if that can be handled here.  If using a single driver system
 # with `netns`, perhaps we can compare output after it is written to a file
 
+loop=1
+interval=5
+
+# Parse any options
+while getopts 'l:i:' OPTION
+do
+  case $OPTION in
+  l)  loop="$OPTARG"
+      ;;
+  i)  interval="$OPTARG"
+      ;;
+  ?)  printf "Usage: %s:[-i <interval_sec>] [-l <loop number>] args\n" $(basename $0) >&2
+      exit 2
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+# Parse any remaining arguments
+
 rec="0100001C0010000080080010ABCDABCDABCDABCD1234567812345678";
 
 echo -n > inputfile
-tail -f inputfile | nc 192.168.10.117 5555 &
+tail -f inputfile | nc "localhost" "5555" &
 echo $rec;
-while true
+while ((loop > 0))
 do
     echo -n $rec | xxd -r -p >> inputfile;
-    sleep 5;
+    sleep $interval;
+    let loop--
 done
 
 
