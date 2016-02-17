aee1f7987aa65f500b1acc8cd639c14715ef9160
Daniel Alvarez <js9045@gmail.com>
2016-02-16 22:58:02 -0800

    Add logging of records sent capability for later comparing with received records

--- sendrecords.sh
+++ sendrecords.sh
@@ -13,41 +13,44 @@
 
 # Default parameters, can be overriden through options
 cflag=
-loop=1
+log=
+repeat=1
 interval=1
 host="localhost"
 dport="5555"
 sport_arg=
 tcp_pid=
+rec="0100001c0010000080080010abcdabcdabcdabcd1234567812345678";
 
 # Parse any options
-while getopts 'l:i:h:s:d:c:' OPTION
+while getopts 'lr:i:h:s:d:c:' OPTION
 do
   case $OPTION in
+  l)  log=1
+      logfile="sent_${$}.hex"
+      echo -n > $logfile
+      ;;
   h)  host="$OPTARG"
       ;;
   d)  dport="$OPTARG"
       ;;
   s)  sport_arg="-p $OPTARG"
       ;;
-  l)  loop="$OPTARG"
+  r)  repeat="$OPTARG"
       ;;
   i)  interval="$OPTARG"
       ;;
   c)  fileout="$OPTARG"
       cflag=1
       ;;
-  ?)  printf "Usage: %s:[-i <interval_sec>] [-l <loop number>] \
-              [-h <dest host>] [-s <src port>] [-d <dest port>] \
-              [-c <capture-file>] args\n" $(basename $0) >&2
+  ?)  printf "Usage: %s:[-l] [-i <interval_sec>] [-r <repeat number>] [-h <dest host>] [-s <src port>] [-d <dest port>] [-c <capture-file>] args\n" $(basename $0) >&2
       exit 2
       ;;
   esac
 done
 shift $(($OPTIND - 1))
 # Parse any remaining arguments
-
-rec="0100001C0010000080080010ABCDABCDABCDABCD1234567812345678";
+# ...
 
 # If set enable tcpdump capture of the session, needs passwd-less sudo support
 if [ "$cflag" ]
@@ -63,14 +66,17 @@ printf "Calling nc as: nc %s %s %s &\n" $sport_arg $host $dport
 echo -n > inputfile
 tail -f inputfile | nc $sport_arg $host $dport &
 # Store the background process pid
-echo $!
 child=$!
 echo $rec;
-while ((loop > 0))
+while ((repeat > 0))
 do
     echo -n $rec | xxd -r -p >> inputfile;
+    if [ "$log" ]
+    then
+      echo $rec >> $logfile
+    fi
     sleep $interval;
-    let loop--
+    let repeat--
 done
 
 # Clean up background process
