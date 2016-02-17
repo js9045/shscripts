980e11f61e574b2c098cb657a799c4c96335b24d
Daniel Alvarez <js9045@gmail.com>
2016-02-17 12:06:21 -0800

    add send and receive record validation and remote listening server

--- sendrecords.sh
+++ sendrecords.sh
@@ -7,32 +7,34 @@
 #
 # @todo: pass in optional file(s) to use as records
 # @todo: enhance script to send data in multiple sessions
-# @todo: validate received data (as received data is handled elsewhere
 # @todo: deal when payload length > 256 as xxd only supports up to 256 columns
-# @todo: support non localhost receiver
-# not clear if that can be handled here.  If using a single driver system
-# with `netns`, perhaps we can compare output after it is written to a file
+# @todo: support active remote host
 
-# Default parameters, can be overriden through options
+# Flags and default parameters, can be overriden through options
 cflag=
-log=
+log=0
 repeat=1
 interval=1
 host="localhost"
 dport="5555"
 sport_arg=
 tcp_pid=
+debug=0
 rec="0100001c0010000080080010abcdabcdabcdabcd1234567812345678";
 len=28
 login="sent_${$}.hex"
 logout="output_${$}.hex"
 
 # Parse any options
-while getopts 'lr:i:h:s:d:c:' OPTION
+while getopts 'Dlr:i:h:s:d:c:' OPTION
 do
   case $OPTION in
+  D)  debug=1
+      # for local debugging/development of script through localhost.
+      # with the remote host in a listening connection.  For real Arbiter
+      # testing the remote also makes an active connection.
+      ;;
   l)  log=1
-      echo -n > $login
       ;;
   h)  host="$OPTARG"
       ;;
@@ -56,31 +58,38 @@ shift $(($OPTIND - 1))
 # Parse any remaining arguments
 # ...
 
-# Set up the listening end of the session
+# Set up the remote session
 if [ "$host" == "localhost" ] 
 then
-  printf "Start listening socket: nc -l %s | xxd -p -c %s &\n" $dport $len
-  if [ "$log" ]
-  then
-    nc -l $dport | xxd -p -c $len > $logout &
-  else 
-    nc -l $dport | xxd -p -c $len &
-  fi
+  printf "Start listening socket: nc -d -l %s | xxd -p -c %s &\n" $dport $len
+  [ $log = 1 ] && { nc -d -l $dport | xxd -p -c $len > $logout & } \
+               || { nc -d -l $dport | xxd -p -c $len & }
 else
-  printf "Start remote listening socket: ssh %s nc -l %s | xxd -p -c %s &\n" $host $dport $len
-  ssh $host "nc -l $dport | xxd -p -c $len > $logout &"
+  # remote host
+  # set up logging regardless of parameter flag
+  log=1
+
+  if [ $debug = 1 ]
+  then
+    printf "Start remote listening socket: ssh %s nc -l %s | xxd -p -c %s &\n" $host $dport $len
+    ssh $host "sh -c 'nc -d -l $dport | xxd -p -c $len >$logout 2>&1 &'"
+  else
+    printf "Start remote active socket: ssh %s nc -l %s | xxd -p -c %s &\n" $host $dport $len
+  fi
 fi
 
 # If set enable tcpdump capture of the session, needs passwd-less sudo support
 if [ "$cflag" ]
 then
-#  sudo tcpdump -i lo0 -n -w $fileout dst port $dport &
+# sudo tcpdump -i lo0 -n -w $fileout dst port $dport &
   sudo tcpdump -i lo0 -n dst port $dport > $fileout &
   tcp_pid=$!
 fi
 sleep 1
 
 printf "Start transmit socket: nc %s %s %s &\n" $sport_arg $host $dport
+[ $log = 1 ] && echo -n > $login
+
 echo -n > inputfile
 tail -f inputfile | nc $sport_arg $host $dport &
 # Store the background process pid
@@ -88,22 +97,36 @@ child=$!
 printf "%s\n------\n" $rec
 while ((repeat > 0))
 do
+    echo -n "."
     echo -n $rec | xxd -r -p >> inputfile;
-    if [ "$log" ]
-    then
-      echo $rec >> $login
-    fi
+    [ $log = 1 ] && echo $rec >> $login
     sleep $interval;
     let repeat--
 done
+echo
 
 # Clean up background process
-rm inputfile
 kill -9 $child $((child - 1))
 if [ "$tcp_pid" ]
 then 
   sudo kill $tcp_pid
 fi
+rm inputfile
+
+sleep 3
+# Validate the data
+if [ $log = 1 ] 
+then
+  if [ "$host" != "localhost" ] 
+  then
+    # Get the output file from remote host
+    scp $host:$logout .
+  fi
+  printf "Diffing input and output records\n"
+  diff $login $logout
+fi
+
+
 
 
 
