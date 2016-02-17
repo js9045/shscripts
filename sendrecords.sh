9ac02717f4555cea8974e3ad252a88d4ecae616a
Daniel Alvarez <js9045@gmail.com>
2016-02-16 21:23:12 -0800

    add initial support for packet capture

--- sendrecords.sh
+++ sendrecords.sh
@@ -12,14 +12,16 @@
 # with `netns`, perhaps we can compare output after it is written to a file
 
 # Default parameters, can be overriden through options
+cflag=
 loop=1
 interval=1
 host="localhost"
 dport="5555"
 sport_arg=
+tcp_pid=
 
 # Parse any options
-while getopts 'l:i:h:s:d:' OPTION
+while getopts 'l:i:h:s:d:c:' OPTION
 do
   case $OPTION in
   h)  host="$OPTARG"
@@ -32,8 +34,12 @@ do
       ;;
   i)  interval="$OPTARG"
       ;;
+  c)  fileout="$OPTARG"
+      cflag=1
+      ;;
   ?)  printf "Usage: %s:[-i <interval_sec>] [-l <loop number>] \
-              [-h <dest host>] [-s <src port>] [-d <dest port>] args\n" $(basename $0) >&2
+              [-h <dest host>] [-s <src port>] [-d <dest port>] \
+              [-c <capture-file>] args\n" $(basename $0) >&2
       exit 2
       ;;
   esac
@@ -43,11 +49,21 @@ shift $(($OPTIND - 1))
 
 rec="0100001C0010000080080010ABCDABCDABCDABCD1234567812345678";
 
-#printf "Calling nc as: nc %s %s %s &\n" $sport_arg $host $dport
+# If set enable tcpdump capture of the session, needs passwd-less sudo support
+if [ "$cflag" ]
+then
+#  sudo tcpdump -i lo0 -n -w $fileout dst port $dport &
+  sudo tcpdump -i lo0 -n dst port $dport > $fileout &
+  tcp_pid=$!
+fi
+sleep 1
 
+echo $!
+printf "Calling nc as: nc %s %s %s &\n" $sport_arg $host $dport
 echo -n > inputfile
 tail -f inputfile | nc $sport_arg $host $dport &
 # Store the background process pid
+echo $!
 child=$!
 echo $rec;
 while ((loop > 0))
@@ -59,6 +75,10 @@ done
 
 # Clean up background process
 kill -9 $child $((child - 1))
+if [ "$tcp_pid" ]
+then 
+  sudo kill $tcp_pid
+fi
 
 
 
